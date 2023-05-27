9b43a885e2dfb1a4edcc4fe0f1001b6cf6d706c1
Mohammad Bahmani <mohammadmandegar2019@gmail.com>
2023-05-27 12:33:28 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,92 @@
+// Programming Language
+
+// High level        JavaScript , Python , Java
+// Medium
+// Machine Language    010001111000100
+
+// Two
+// Interpreter  line by line    JavaScript  Python
+// Compiler     whole           Java
+
+// ==========================================================================================
+//                                JS Fundamentals
+// ==================================================================================
+console.log("Hello World");
+// =========================== Data
+// "Mohammad"
+// 17
+// 0727283812
+// true
+// false
+// undefined
+// null
+
+console.log("Hussain");
+console.log(17);
+// =========================== Variable
+let student = "MOhammad";
+console.log(student);
+let schoolStudentAge = 18;
+console.log(schoolStudentAge);
+student = "Ahmad";
+console.log(student);
+// =========================== let, const, var
+var age = 19;
+age = 21;
+console.log(age);
+
+let height = 1.8;
+height = 1.85;
+console.log(height);
+
+const firstName = "Mujib";
+console.log(firstName);
+
+// =========================== Data Type
+// 7 Data Types
+/* 
+1. String   "Mohammad" "Car " "D" "282828" "true"
+2. Number    89 90 45  83  23.4  58.9  3.141515  
+3. Boolean   true  false   false   false   false
+4. Undefined  undefined
+5. Null      null
+6. Big Int 
+7. Symbol    
+
+*/
+let lastName;
+console.log(typeof firstName);
+console.log(typeof "23");
+console.log(typeof 23);
+console.log(typeof lastName);
+console.log(typeof null);
+
+// =========================== Operator
+
+// =========================== Arthmetic Operator
+console.log(10 + 20);
+console.log(10 - 20);
+console.log(10 * 20);
+console.log(10 / 20);
+console.log(10 % 20);
+console.log(10 ** 20);
+
+console.log(20 % 6);
+console.log(2 ** 3);
+
+// =========================== Assignment operator
+let add = 10;
+add = add + 20;
+console.log(add);
+add += 20;
+console.log(add);
+add = add - 10;
+add -= 10;
+add = add * 2;
+add *= 2;
+add = add / 3;
+add /= 3;
+// =============================
+console.log(23 + 4 / 2 > 21 - 3 * 6);
+console.log(23 + 2 > 21 - 18);
+console.log(25 > 3);
